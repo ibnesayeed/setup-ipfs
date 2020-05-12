548d0160ebfcf4d88cf3d4e27a4ab7b84a9d7ae2
Sawood Alam <ibnesayeed@gmail.com>
2020-05-12 03:10:06 -0400

    Add an overall setup test

---
+++ index.test.js
@@ -0,0 +1,12 @@
+const process = require('process')
+const cp = require('child_process')
+const path = require('path')
+
+
+test('main script runs', () => {
+    process.env['INPUT_IPFS_VERSION'] = 'v0.5.1'
+    process.env['RUNNER_TEMP'] = path.join(__dirname, 'TEMP')
+    process.env['RUNNER_TOOL_CACHE'] = path.join(__dirname, 'CACHE')
+    const ip = path.join(__dirname, 'index.js')
+    console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString())
+})
